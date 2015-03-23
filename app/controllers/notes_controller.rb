5d37798bbab8ec7e8564cfc00ef4063c0f3b6032
Ahn Jungho <ahnjungho@ahnjungho.org>
2015-03-23 17:28:08 +0900

    add a note controller and some note views

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,17 @@
+class NotesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new 
+  end
+
+  def create
+    @note = Note.new(params.require(:note).permit(:body))
+    @note.user = current_user
+    @note.save
+    redirect_to @note
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+end
